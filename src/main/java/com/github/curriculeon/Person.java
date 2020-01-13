0cfeacd9ce012021f5983baeb6251f6a77fa8bc0
Susan Tabassum <sttabassum93@gmail.com>
2020-01-12 19:38:38 -0500

    Created Person and TestPerson

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,5 +1,29 @@
 package com.github.curriculeon;
 
 public class Person {
+    final long id;
+    private String name;
+
+    public Person(){
+        this.id = 0;
+        this.name = "";
+    }
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 
 }
--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -1,5 +1,13 @@
 package com.github.curriculeon;
 
+import org.junit.Test;
+
 public class TestPerson {
+    private Person person = new Person(38572749, "Sam");
+
+    @Test
+    public void testConstructor(){
+
+    }
 
 }
