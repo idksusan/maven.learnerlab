381d79122f78e89988eebb32052f1c1891d5f433
Susan Tabassum <sttabassum93@gmail.com>
2020-01-12 20:14:30 -0500

    Started TestStudent

---
+++ src/test/java/com/github/curriculeon/TestStudent.java
@@ -0,0 +1,23 @@
+package com.github.curriculeon;
+
+import org.junit.Test;
+
+public class TestStudent {
+    final long id = 38572749;
+    final String name = "Sam";
+
+    @Test
+    public void testImplementation(){
+
+    }
+
+    @Test
+    public void testInheritance(){
+
+    }
+
+    @Test
+    public void testLearn(){
+
+    }
+}
