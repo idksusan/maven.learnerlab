84c22a35e357b505d04bb31ed65bf3ea8eaf6d2a
Susan Tabassum <sttabassum93@gmail.com>
2020-01-12 19:54:54 -0500

    Completed Student class

--- src/main/java/com/github/curriculeon/Student.java
+++ src/main/java/com/github/curriculeon/Student.java
@@ -1,13 +1,15 @@
 package com.github.curriculeon;
 
 public class Student extends Person implements Learner {
+    private double totalStudyTime;
+
     @Override
     public void learn(double numberOfHours) {
-
+        totalStudyTime += numberOfHours;
     }
 
     @Override
     public Double getTotalStudyTime() {
-        return null;
+        return totalStudyTime;
     }
 }
