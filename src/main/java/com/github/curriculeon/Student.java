a05a69bf321ea25b4cdc82a5f2187c3a9559fc66
Susan Tabassum <sttabassum93@gmail.com>
2020-01-12 19:49:08 -0500

    Completed TestPerson

---
+++ src/main/java/com/github/curriculeon/Student.java
@@ -0,0 +1,13 @@
+package com.github.curriculeon;
+
+public class Student extends Person implements Learner {
+    @Override
+    public void learn(double numberOfHours) {
+
+    }
+
+    @Override
+    public Double getTotalStudyTime() {
+        return null;
+    }
+}
--- src/test/java/com/github/curriculeon/TestPerson.java
+++ src/test/java/com/github/curriculeon/TestPerson.java
@@ -2,12 +2,17 @@ package com.github.curriculeon;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestPerson {
-    private Person person = new Person(38572749, "Sam");
+    final long id = 38572749;
+    final String name = "Sam";
+    private Person person = new Person(id, name);
 
     @Test
     public void testConstructor(){
-
+        assertEquals(person.getId(), id);
+        assertEquals(person.getName(), name);
     }
 
 }
